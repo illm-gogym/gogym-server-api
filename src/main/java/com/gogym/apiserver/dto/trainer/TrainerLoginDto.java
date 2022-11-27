b40b924f61413724af3e8b1ac6b8375280f67451
moonmoon.kim <visitant2@naver.com>
2022-11-27 22:14:52 +0900

    changed snake

--- src/main/java/com/gogym/apiserver/dto/trainer/TrainerLoginDto.java
+++ src/main/java/com/gogym/apiserver/dto/trainer/TrainerLoginDto.java
@@ -1,5 +1,7 @@
 package com.gogym.apiserver.dto.trainer;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Getter;
 
 import javax.validation.constraints.Email;
@@ -7,6 +9,7 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Getter
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class TrainerLoginDto {
 
     @NotNull
