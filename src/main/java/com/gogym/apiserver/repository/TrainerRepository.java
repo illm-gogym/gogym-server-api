6dc46f075733d9afe89fc9eeff8a7e1e9832f384
moonmoon.kim <visitant2@naver.com>
2022-12-18 03:29:05 +0900

    트레이너 목록 가져오기 개발

--- src/main/java/com/gogym/apiserver/repository/TrainerRepository.java
+++ src/main/java/com/gogym/apiserver/repository/TrainerRepository.java
@@ -1,10 +1,16 @@
 package com.gogym.apiserver.repository;
 
+import com.gogym.apiserver.dto.reservation.wrapper.ReservationWrapper;
 import com.gogym.apiserver.entity.Trainer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface TrainerRepository extends JpaRepository<Trainer, String> {
     Optional<Trainer> findByTrainerId(String trainerId);
+
+    @Query("SELECT trainer FROM Trainer trainer WHERE trainer.gymId IN (SELECT t.gymId FROM Trainer t WHERE t.trainerId = :trainerId) ORDER BY trainer.name")
+    List<Trainer> getTrainersByTrainerId(String trainerId);
 }
