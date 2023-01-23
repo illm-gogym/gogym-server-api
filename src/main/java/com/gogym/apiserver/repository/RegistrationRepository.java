ded94510ca7f77302132a9a04992bf5cf8e82dd2
ds <dskim@logiall.com>
2023-01-23 21:55:33 +0900

    [ds] 수강권 업데이트

--- src/main/java/com/gogym/apiserver/repository/RegistrationRepository.java
+++ src/main/java/com/gogym/apiserver/repository/RegistrationRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface RegistrationRepository extends JpaRepository<Registration, Long> {
 
@@ -12,7 +13,10 @@ public interface RegistrationRepository extends JpaRepository<Registration, Long
     Long getRegistrationIdByTrainerIdAndUserPhone(String trainerId, String userPhone);
 
     @Query("SELECT r FROM Registration r WHERE r.userPhone = :userPhone")
-    List<Registration> getRegistrationByUserPhone(String userPhone);
+    List<Registration> getRegistrationByUserPhoneList(String userPhone);
+
+    @Query("SELECT r FROM Registration r WHERE r.userPhone = :userPhone")
+    Optional<Registration> getRegistrationByUserPhone(String userPhone);
 
     @Query("SELECT r FROM Registration r WHERE r.trainerId = :trainerId")
     List<Registration> getRegistrationByTrainerId(String trainerId);
